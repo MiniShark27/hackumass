023dfae830123478910dcff35766146c36a42822
MiniShark27 <kobifalus@gmail.com>
2021-11-07 04:07:40 -0500

    j

--- pi/gpio-pi.js
+++ pi/gpio-pi.js
@@ -1,30 +1,23 @@
 const Gpio = require("pigpio").Gpio;
+const io = require("socket.io-client");
+const socket = io.connect("http://skeeball.croissant.one:9000");
 
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-const MICROSECDONDS_PER_CM = 1e6 / 34321;
-const THRESHOLD = 7;
+// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius divided by 2 for 2 ways
+const MICROSECDONDS_PER_CM = 1e6 / 34321 / 2;
+const SCORE_DELAY = 1000;
 const trigger = new Gpio(23, { mode: Gpio.OUTPUT });
+trigger.digitalWrite(0); // Make sure trigger is low
+let lastScore = new Date();
 const info = [
   { pin: 24, score: 100, echo: null, threshold: 7 },
   { pin: 25, score: 50, echo: null, threshold: 7 },
   { pin: 22, score: 20, echo: null, threshold: 7 },
   { pin: 27, score: 10, echo: null, threshold: 7 },
 ];
+
 info.forEach(
   x => (x.echo = new Gpio(x.pin, { mode: Gpio.INPUT, alert: true }))
 );
-
-const SCORE_DELAY = 1000;
-
-trigger.digitalWrite(0); // Make sure trigger is low
-
-let throttle = false;
-
-let lastScore = new Date();
-
-const io = require("socket.io-client");
-
-const socket = io.connect("http://skeeball.croissant.one:9000");
 
 socket.on("connect", () => {
   console.log("Connected to server");
@@ -33,26 +26,21 @@ socket.on("connect", () => {
 const watchHCSR04 = () => {
   let startTick;
   info.forEach((x, i) => {
-    // x.echo.glitchFilter(500)
     x.echo.on("alert", (level, tick) => {
       if (level == 1) {
         startTick = tick;
       } else {
         const endTick = tick;
         const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-        const temp = diff / 2 / MICROSECDONDS_PER_CM;
+        const temp = diff / MICROSECDONDS_PER_CM;
         console.log(`Sensor ${i}: ${temp}`);
-        if (temp < x.threshold && !throttle) {
+        if (temp < x.threshold) {
           console.log(`Sensor ${i}: ${temp} < ${x.threshold}`);
           if (new Date() - lastScore > SCORE_DELAY) {
             lastScore = new Date()
             socket.emit("score", {
               points: x.score,
             });
-            throttle = true;
-            setTimeout(() => {
-              throttle = false;
-            }, 1000);
           }
         }
       }
