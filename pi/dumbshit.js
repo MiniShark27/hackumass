aa453f49954fe2bb865e96082337ec2feeef449d
arhammusheer <arhammusheer@gmail.com>
2021-11-07 02:36:41 -0500

    works

--- pi/dumbshit.js
+++ pi/dumbshit.js
@@ -2,13 +2,21 @@ const Gpio = require("pigpio").Gpio;
 
 // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
 const MICROSECDONDS_PER_CM = 1e6 / 34321;
-
+const THRESHOLD = 6;
 const trigger = new Gpio(23, { mode: Gpio.OUTPUT });
 const echo0 = new Gpio(24, { mode: Gpio.INPUT, alert: true });
 const echo1 = new Gpio(25, { mode: Gpio.INPUT, alert: true });
 const echo2 = new Gpio(22, { mode: Gpio.INPUT, alert: true });
 const echo3 = new Gpio(27, { mode: Gpio.INPUT, alert: true });
 trigger.digitalWrite(0); // Make sure trigger is low
+
+const io = require("socket.io-client");
+
+const socket = io.connect("http://skeeball.croissant.one:9000");
+
+socket.on("connect", () => {
+	console.log("Connected to server");
+});
 
 const watchHCSR04 = () => {
   let startTick;
@@ -19,7 +27,11 @@ const watchHCSR04 = () => {
     } else {
       const endTick = tick;
       const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      console.log(`Sensor 0: ${diff / 2 / MICROSECDONDS_PER_CM}`);
+      if (diff / 2 / MICROSECDONDS_PER_CM < THRESHOLD) {
+        socket.emit("score",{
+          points: 100
+        })
+      }
     }
   });
 
@@ -30,6 +42,11 @@ const watchHCSR04 = () => {
       const endTick = tick;
       const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
       console.log(`Sensor 1: ${diff / 2 / MICROSECDONDS_PER_CM}`);
+      if (diff / 2 / MICROSECDONDS_PER_CM < THRESHOLD) {
+        socket.emit("score",{
+          points: 50
+        })
+      }
     }
   });
 
@@ -40,6 +57,11 @@ const watchHCSR04 = () => {
       const endTick = tick;
       const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
       console.log(`Sensor 2: ${diff / 2 / MICROSECDONDS_PER_CM}`);
+      if (diff / 2 / MICROSECDONDS_PER_CM < THRESHOLD) {
+        socket.emit("score",{
+          points: 20
+        })
+      }
     }
   });
 
@@ -50,6 +72,11 @@ const watchHCSR04 = () => {
       const endTick = tick;
       const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
       console.log(`Sensor 3: ${diff / 2 / MICROSECDONDS_PER_CM}`);
+      if (diff / 2 / MICROSECDONDS_PER_CM < THRESHOLD) {
+        socket.emit("score",{
+          points: 10
+        })
+      }
     }
   });
 };
