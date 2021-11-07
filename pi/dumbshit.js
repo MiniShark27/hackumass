fdd4efdc1b4a321ff6c0e7839526748b05e2e456
arhammusheer <arhammusheer@gmail.com>
2021-11-07 02:27:43 -0500

    added sensor 4

--- pi/dumbshit.js
+++ pi/dumbshit.js
@@ -1,19 +1,19 @@
-const Gpio = require('pigpio').Gpio;
+const Gpio = require("pigpio").Gpio;
 
 // The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-const MICROSECDONDS_PER_CM = 1e6/34321;
+const MICROSECDONDS_PER_CM = 1e6 / 34321;
 
-const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
-const echo0 = new Gpio(24, {mode: Gpio.INPUT, alert: true});
-const echo1 = new Gpio(25, {mode: Gpio.INPUT, alert: true});
-const echo2 = new Gpio(22, {mode: Gpio.INPUT, alert: true});
-
+const trigger = new Gpio(23, { mode: Gpio.OUTPUT });
+const echo0 = new Gpio(24, { mode: Gpio.INPUT, alert: true });
+const echo1 = new Gpio(25, { mode: Gpio.INPUT, alert: true });
+const echo2 = new Gpio(22, { mode: Gpio.INPUT, alert: true });
+const echo3 = new Gpio(27, { mode: Gpio.INPUT, alert: true });
 trigger.digitalWrite(0); // Make sure trigger is low
 
 const watchHCSR04 = () => {
   let startTick;
 
-  echo0.on('alert', (level, tick) => {
+  echo0.on("alert", (level, tick) => {
     if (level == 1) {
       startTick = tick;
     } else {
@@ -23,7 +23,7 @@ const watchHCSR04 = () => {
     }
   });
 
-	echo1.on('alert', (level, tick) => {
+  echo1.on("alert", (level, tick) => {
     if (level == 1) {
       startTick = tick;
     } else {
@@ -33,13 +33,23 @@ const watchHCSR04 = () => {
     }
   });
 
-	echo2.on('alert', (level, tick) => {
+  echo2.on("alert", (level, tick) => {
     if (level == 1) {
       startTick = tick;
     } else {
       const endTick = tick;
       const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
       console.log(`Sensor 2: ${diff / 2 / MICROSECDONDS_PER_CM}`);
+    }
+  });
+
+  echo3.on("alert", (level, tick) => {
+    if (level == 1) {
+      startTick = tick;
+    } else {
+      const endTick = tick;
+      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+      console.log(`Sensor 3: ${diff / 2 / MICROSECDONDS_PER_CM}`);
     }
   });
 };
